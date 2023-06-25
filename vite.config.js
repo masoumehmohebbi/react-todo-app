5c628787128c8189537b1ef7ad5c046a05dfb210
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-06-25 12:32:23 +0330

    Remove: mimeTypes script

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  mimeTypes: {
-    '.js': 'application/javascript',
-    '.jsx': 'application/javascript'
-  },
-
-  base: "/react-todo-app/",
+  base: "/react-todo-app",
   plugins: [react()]
 })
 
