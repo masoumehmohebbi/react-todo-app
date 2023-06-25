2d59e96f0ad5bff75a8493b2696def091eae078a
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-06-25 09:46:24 +0330

    Add MIME Type Script

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  mimeTypes: {
+    '.jsx': 'application/javascript'
+  },
+
   base: "/react-todo-app/",
   plugins: [react()]
 })
+
