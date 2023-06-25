33c4afd8e449ea9126473371bb41a94096b39954
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-06-25 09:50:42 +0330

    Added: mimeTypes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   mimeTypes: {
+    '.js': 'application/javascript',
     '.jsx': 'application/javascript'
   },
 
